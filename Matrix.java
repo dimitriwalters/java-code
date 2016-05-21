7204e899108caa5d86cb72644a6dd04ffb46f341
Dimitri Walters <me@dimi.io>
2016-05-21 17:19:33 -0400

    Implement in-place methods

--- Matrix.java
+++ Matrix.java
@@ -40,12 +40,38 @@ public class Matrix {
     return newMatrix;
   }
 
+  // in-place
+  public static void reverseRows(int[][] matrix) {
+    for (int row=0; row<matrix.length; row++) {
+      for (int col=0; col<matrix[0].length/2; col++) {
+        int temp = matrix[row][col];
+        matrix[row][col] = matrix[row][matrix[0].length - 1 - col];
+        matrix[row][matrix[0].length - 1 - col] = temp;
+      }
+    }
+  }
+
+  // in-place (N x N matrix)
+  public static void transposeInPlace(int[][] matrix) {
+    for (int row=0; row<matrix.length; row++) {
+      for (int col=row+1; col<matrix.length; col++) {
+        int temp = matrix[row][col];
+        matrix[row][col] = matrix[col][row];
+        matrix[col][row] = temp;
+      }
+    }
+  }
+
+  // in-place (N x N matrix)
+  public static void rotateInPlace(int[][] matrix) {
+    transposeInPlace(matrix);
+    reverseRows(matrix);
+  }
+
   public static void main(String[] args) {
-    int[][] matrix1 = {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}, {19, 20, 21}};
+    int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     printMatrix(matrix1);
-    int[][] matrix2 = transpose(matrix1);
-    printMatrix(matrix2);
-    int[][] matrix3 = rotate90(matrix1);
-    printMatrix(matrix3);
+    rotateInPlace(matrix1);
+    printMatrix(matrix1);
   }
 }
